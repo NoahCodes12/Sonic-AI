a76c15aebd41ab020acff921140507f57e79513f
NoahCodes12 <noah.planner.1@gmail.com>
2024-06-05 19:53:30 +0200

    Send Button Look Update

--- GUI.py
+++ GUI.py
@@ -1,11 +1,9 @@
 import customtkinter as ctk
 #import tkinter as tk
-import funcs as fs
-import CTkMessagebox as ctkm
-from PIL import Image as img
-from PIL import ImageTk as imgt
-import random
-    
+#def User_Input():
+
+#def AI_Answer():
+
 root = ctk.CTk()
 root.geometry('600x440')
 root.title('Sonic-AI')
@@ -15,10 +13,10 @@ root.iconbitmap('Bild.ico')
 label=ctk.CTkLabel(root, text="Sonic-AI", font=('Arial', 36))
 label.pack(padx=20, pady=20)
 
-textbox=ctk.CTkTextbox(root, width=1000, height=41, font=('Arial', 16,), fg_color="white", text_color="black")
+textbox=ctk.CTkTextbox(root, width=900, height=41, font=('Arial', 16,), fg_color="white", text_color="black")
 textbox.place(x=200, y=600)
 button=ctk.CTkButton(root, width=100, height=41, text="Senden", font=('Arial', 18))
-button.place(x=1200,y=600)
+button.place(x=1105,y=600)
 def exit():
     root.quit()
 Exit=ctk.CTkButton(root, text="Quit", command=exit)
