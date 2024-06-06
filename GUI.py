61a8d3cdc7e7278bc40d7f2d7ea552ca913812e3
NoahCodes12 <noah.planner.1@gmail.com>
2024-06-06 14:42:13 +0200

    GUI Save Funtionality USER

--- GUI.py
+++ GUI.py
@@ -1,32 +1,31 @@
 import customtkinter as ctk
-#from PIL import Image
 from PIL import Image,ImageTk
 # import rsvg,cairo 
 import tkinter as tk
-from tkinter import font
-import pygame
-#def User_Input():
+import json
 
-#def AI_Answer():
+FILE_NAME = "vlt.json"
+USER_INPUT = []
+USER_NAME = "Test"
 
+def User_Input():
+    UserGet=textbox.get("1.0",'end-1c')
+    print(UserGet)
+    USER_INPUT.append(UserGet)
+    i = 0
+    for element in USER_INPUT:
+        print(f"{USER_NAME}: {USER_INPUT[i]}")
+        with open(FILE_NAME, 'a') as obj:
+            data = json.dump(f"{USER_NAME} : {USER_INPUT[i]},", obj)
+        # test
+        # UserText = ctk.CTkLabel(scroll, width=50, height=50, font=("Arial", 16), text=USER_INPUT)
+        # UserText.pack()
+    i+=1
 
 root = ctk.CTk()
 root.geometry('600x440')
 root.title('Sonic-AI')
 root.iconbitmap('Bild.ico')
-
-
-
-# saving image to variable
-dark_image = Image.open("sonic-running-run.png")
-# using ctkimage element to add image to
-bild = ctk.CTkImage(dark_image=dark_image, size= (80, 80))
-
-# putting the image inside a label
-label = ctk.CTkLabel(root, image=bild, text="")
-# placing the label
-label.place(x=1000, y=30)
-
 image_send= ImageTk.PhotoImage(Image.open('mingcute--send-fill (1).png'))
 # self.root.iconbitmap('icons8-file-folder-48.png')
 label=ctk.CTkLabel(root, text="Sonic-AI", font=('Arial', 100))
@@ -35,7 +34,7 @@ label.pack(padx=20, pady=20)
 textbox=ctk.CTkTextbox(root, width=900, height=50, font=('Arial', 16,), fg_color="white", text_color="black")
 textbox.place(x=250, y=600)
 
-button=ctk.CTkButton(root, width=100, height=50,text="", image=image_send, font=('Arial', 18))
+button=ctk.CTkButton(root, width=100, height=50,text="", image=image_send, font=('Arial', 18), command=User_Input)
 button.place(x=1155,y=600)
 
 def exit():
@@ -53,11 +52,7 @@ scroll.place(x=250, y=188)
 def combobox_callback(choice):
     print("combobox dropdown clicked:", choice)
 
-combobox = ctk.CTkComboBox(root,values=["practice", "option2"],command=combobox_callback, width=200, height=50)
+combobox = ctk.CTkComboBox(root,values=["practice", "option2"],command=combobox_callback, width=150, height=50)
 
 combobox.place(x=20, y=188)
 root.mainloop()
-
-
-
-
