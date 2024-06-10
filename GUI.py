725ddb1db742926480ea58adaafc75493438831c
NicoN17 <nicograuf45@gmail.com>
2024-06-10 09:36:40 +0200

    GUI Update V8

--- GUI.py
+++ GUI.py
@@ -41,6 +41,7 @@ root.geometry('600x440')
 root.title('Sonic-AI')
 root.iconbitmap('Bild.ico')
 image_send= ImageTk.PhotoImage(Image.open('mingcute--send-fill (1).png'))
+image_speak=ImageTk.PhotoImage(Image.open('iconoir--microphone-solid (1).png'))
 
 # saving image to variable
 dark_image = Image.open("sonic-running-run.png")
@@ -52,8 +53,13 @@ label = ctk.CTkLabel(root, image=bild, text="")
 # placing the label
 label.place(x=1000, y=30)
 
-speak = ctk.CTkButton(root, width=50, height=50,text="", image=image_send, font=('Arial', 18), command=ai.listen_and_recognize) # letting recognize function handle it
-speak.place(x=1210,y=600)
+
+# button to handle sending user input
+send=ctk.CTkButton(root, width=50, height=50,text="", image=image_send, font=('Arial', 18), command=UserInput)
+send.place(x=1155,y=590)
+
+speak = ctk.CTkButton(root, width=50, height=50,text="", image=image_speak, font=('Arial', 18), command=ai.listen_and_recognize) # letting recognize function handle it
+speak.place(x=1210,y=590)
 
 # Title
 label=ctk.CTkLabel(root, text="Sonic-AI", font=('Arial', 100))
@@ -61,18 +67,25 @@ label.pack(padx=20, pady=20)
 
 # text box for user and ai input
 textbox=ctk.CTkTextbox(root, width=900, height=50, font=('Arial', 16,), fg_color="white", text_color="black")
-textbox.place(x=250, y=600)
+textbox.place(x=250, y=590)
 
-# button to handle sending user input
-button=ctk.CTkButton(root, width=50, height=50,text="", image=image_send, font=('Arial', 18), command=UserInput)
-button.place(x=1155,y=600)
+
+
+blackjack=ctk.CTkButton(root, text="Blackjack", width=150, height=50, font=('Arial', 30))
+blackjack.place(x=20, y=315)
+
+def new_chat():
+    scroll.delete("1.0", ctk.END)
+
+newchat=ctk.CTkButton(root, text="New Chat", width=150, height=50, font=('Arial', 30), command=new_chat)
+newchat.place(x=20, y=380)
 
 # exit function for exit button
 def exit():
     root.quit()
 # exit button
 Exit=ctk.CTkButton(root, text="Quit",font=("Arial", 30), command=exit, width=150, height=50)
-Exit.place(x=30, y=40)
+Exit.place(x=20, y=40)
 
 # login button
 Login=ctk.CTkButton(root, text="Login",font=("Arial", 30), width=150, height=50)
@@ -80,7 +93,7 @@ Login.place(x=1350, y=40)
 
 # scroll = ctk.CTkScrollableFrame(root, width=980, height=390, fg_color="white")
 # scroll.place(x=250, y=188)
-scroll = ctk.CTkTextbox(root, width=1000, height=390, fg_color="white", text_color="lime", font=("Helvetica", 20))
+scroll = ctk.CTkTextbox(root, width=1010, height=390, fg_color="white", text_color="lime", font=("Helvetica", 20))
 scroll.place(x=250, y=188)
 
 #
@@ -91,6 +104,9 @@ def combobox_callback(choice):
 # dropdown menu
 combobox = ctk.CTkComboBox(root,values=["chat", "practice"],command=combobox_callback, width=150, height=50)
 combobox.place(x=20, y=188)
+
+english_knowledge = ctk.CTkComboBox(root,values=["Level 1", "Level 2", "Level 3"],command=combobox_callback, width=150, height=50)
+english_knowledge.place(x=20, y=250)
 
 
 
