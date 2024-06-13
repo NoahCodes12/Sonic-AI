c1d0fab395c6f6e83e78a9a4ec887c7a502c58e9
OrdianHD <Jonasgt93@gmail.com>
2024-06-13 08:37:01 +0200

    bug fixes

--- Save.py
+++ Save.py
@@ -2,35 +2,41 @@ import json
 import os
 from colorama import Fore, Style
 
-# Function to collect user information
-def collect_user_info(name, age):
-    users = []
-    while True:
-        # Save the user information in a dictionary
-        user = {
-            "name": name,
-            "age": age,
-        }
-        users.append(user)
-        return users
+filename = "user_data.json"
 
 # Function to save the user information to a JSON file
 def save_to_json(new_data, filename="user_data.json"):
-    # Check if the file exists
-    if os.path.exists(filename):
-        # Read the existing data
-        with open(filename, "r") as file:
-            existing_data = json.load(file)
-    else:
-        existing_data = []
+    try:
+        # Check if the file exists and read the existing data
+        if os.path.exists(filename):
+            print(f"File {filename} exists")
+            with open(filename, "r") as file:
+                existing_data = json.load(file)
+        else:
+            print(f"File {filename} does not exist")
+            existing_data = []
 
-    # Update the existing data with new data
-    for user in new_data:
-        if user not in existing_data:
-            existing_data.append(user)
+        # Update the existing data with new data
+        updated_data = existing_data.copy()
+        for user in new_data:
+            if user not in existing_data:
+                updated_data.append(user)
 
-    # Write the updated data back to the file
-    with open(filename, "w") as file:
-        json.dump(existing_data, file, indent=4)
+        # Write the updated data back to the file
+        with open(filename, "w") as file:
+            json.dump(updated_data, file, indent=4)
+            print(f"{Fore.BLUE}User information {Style.BRIGHT}saved successfully.{Style.RESET_ALL}")
 
-print(f"{Fore.BLUE}User information {Style.BRIGHT}saved successfully.")
+    except Exception as e:
+        print(f"Exception raised: {e}")
+    finally:
+        print("Call ended")
+
+# Function to collect user information
+def collect_user_info(name, age, lvl):
+    user = {
+        "name": name,
+        "age": age,
+        "English Knowledge Level": lvl
+    }
+    save_to_json([user])
